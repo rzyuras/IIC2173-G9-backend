05af61ad5813e3045e113e7c88b8f32327d4229c
Matias Oliva <matiasolivab@uc.cl>
2024-04-18 17:31:15 -0400

    Eslint

--- app/db.js
+++ app/db.js
@@ -1,37 +1,37 @@
-const { Client } = require('pg');
+const { Client } = require("pg");
 
 class Database {
-    constructor(dbname, user, password, host) {
-        this.client = new Client({
-            user: user,
-            host: host,
-            database: dbname,
-            password: password
-        });
-    }
+  constructor(dbname, user, password, host) {
+    this.client = new Client({
+      user: user,
+      host: host,
+      database: dbname,
+      password: password,
+    });
+  }
 
-    async connect() {
-        await this.client.connect();
-    }
+  async connect() {
+    await this.client.connect();
+  }
 
-    async getAllFlights() {
-        const query = `
+  async getAllFlights() {
+    const query = `
             SELECT * FROM flights
         `;
-        const result = await this.client.query(query);
-        return result.rows;
-    }
+    const result = await this.client.query(query);
+    return result.rows;
+  }
 
-    async getFlight(identifier) {
-        const query = `
+  async getFlight(identifier) {
+    const query = `
             SELECT * FROM flights WHERE id = $1
         `;
-        const result = await this.client.query(query, [identifier]);
-        return result.rows[0];
-    }
+    const result = await this.client.query(query, [identifier]);
+    return result.rows[0];
+  }
 
-    async insertFlight(data) {
-        const insertQuery = `
+  async insertFlight(data) {
+    const insertQuery = `
             INSERT INTO flights 
             (departure_airport_name, departure_airport_id, departure_airport_time, 
             arrival_airport_name, arrival_airport_id, arrival_airport_time, 
@@ -40,19 +40,28 @@ class Database {
             VALUES 
             ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
         `;
-        const values = [
-            data.departure_airport_name, data.departure_airport_id, data.departure_airport_time,
-            data.arrival_airport_name, data.arrival_airport_id, data.arrival_airport_time,
-            data.duration, data.airplane, data.airline, data.airline_logo,
-            data.carbon_emissions, data.price, data.currency, data.airline_logo_url
-        ];
-        await this.client.query(insertQuery, values);
-    }
+    const values = [
+      data.departure_airport_name,
+      data.departure_airport_id,
+      data.departure_airport_time,
+      data.arrival_airport_name,
+      data.arrival_airport_id,
+      data.arrival_airport_time,
+      data.duration,
+      data.airplane,
+      data.airline,
+      data.airline_logo,
+      data.carbon_emissions,
+      data.price,
+      data.currency,
+      data.airline_logo_url,
+    ];
+    await this.client.query(insertQuery, values);
+  }
 
-    async close() {
-        await this.client.end();
-    }
+  async close() {
+    await this.client.end();
+  }
 }
 
 module.exports = Database;
-
--- app/main.js
+++ app/main.js
@@ -1,113 +1,130 @@
-const express = require('express');
-const Database = require('./db'); // Asume que tienes un archivo db.js que exporta la clase Database
-require('dotenv').config();
-const moment = require('moment-timezone');
+const express = require("express");
+const Database = require("./db"); // Asume que tienes un archivo db.js que exporta la clase Database
+require("dotenv").config();
+const moment = require("moment-timezone");
 
 // Datos de conexión PostgreSQL
 const pgDbname = process.env.DATABASE_NAME;
 const pgUser = process.env.DATABASE_USER;
 const pgPassword = process.env.DATABASE_PASSWORD;
-const pgHost = 'db'; // O usa 'localhost' según tu configuración local
+const pgHost = "db"; // O usa 'localhost' según tu configuración local
 
 // Crear instancia de Database
 const db = new Database(pgDbname, pgUser, pgPassword, pgHost);
-db.connect()
-
+db.connect();
 
 // Crea una instancia de Express y la almacena en la variable app.
 const app = express();
 app.use(express.json()); // Middleware para parsear JSON
 
-
 class FlightData {
-    constructor(data) {
-        this.price = data.price;
-        this.currency = data.currency;
-        this.carbon_emissions = data.carbon_emissions;
-        this.airline_logo_url = data.airline_logo_url;
-        this.departure_airport_name = data.departure_airport_name;
-        this.departure_airport_id = data.departure_airport_id;
-        this.departure_airport_time = data.departure_airport_time;
-        this.arrival_airport_name = data.arrival_airport_name;
-        this.arrival_airport_id = data.arrival_airport_id;
-        this.arrival_airport_time = data.arrival_airport_time;
-        this.duration = data.duration;
-        this.airplane = data.airplane;
-        this.airline = data.airline;
-        this.airline_logo = data.airline_logo;
-    }
+  constructor(data) {
+    this.price = data.price;
+    this.currency = data.currency;
+    this.carbon_emissions = data.carbon_emissions;
+    this.airline_logo_url = data.airline_logo_url;
+    this.departure_airport_name = data.departure_airport_name;
+    this.departure_airport_id = data.departure_airport_id;
+    this.departure_airport_time = data.departure_airport_time;
+    this.arrival_airport_name = data.arrival_airport_name;
+    this.arrival_airport_id = data.arrival_airport_id;
+    this.arrival_airport_time = data.arrival_airport_time;
+    this.duration = data.duration;
+    this.airplane = data.airplane;
+    this.airline = data.airline;
+    this.airline_logo = data.airline_logo;
+  }
 }
 
-app.get('/flights', async (req, res) => {
-    try {
-        const { page = 1, count = 25, departure, arrival, date } = req.query;
-        const flights = await db.getAllFlights();
-        const actualDatetime = moment().utc();
-        let flightsFiltered = flights;
-    
-        if (departure && !arrival && !date) {
-            flightsFiltered = flights.filter(flight => flight.departure_airport_id === departure);
-        } else if (arrival && !departure && !date) {
-            flightsFiltered = flights.filter(flight => flight.arrival_airport_id === arrival);
-        } else if (date && !departure && !arrival) {
-            flightsFiltered = flights.filter(flight => 
-                moment(flight.departure_airport_time).format("YYYY-MM-DD") === date &&
-                actualDatetime <= moment(flight.departure_airport_time)
-            );
-        } else if (departure && arrival && !date) {
-            flightsFiltered = flights.filter(flight => 
-                flight.departure_airport_id === departure && flight.arrival_airport_id === arrival
-            );
-        } else if (departure && date && !arrival) {
-            flightsFiltered = flights.filter(flight => 
-                flight.departure_airport_id === departure &&
-                moment(flight.departure_airport_time).format("YYYY-MM-DD") === date &&
-                actualDatetime <= moment(flight.departure_airport_time)
-            );
-        } else if (arrival && date && !departure) {
-            flightsFiltered = flights.filter(flight => 
-                flight.arrival_airport_id === arrival &&
-                moment(flight.departure_airport_time).format("YYYY-MM-DD") === date &&
-                actualDatetime <= moment(flight.departure_airport_time)
-            );
-        } else if (departure && arrival && date) {
-            flightsFiltered = flights.filter(flight => 
-                flight.departure_airport_id === departure &&
-                flight.arrival_airport_id === arrival &&
-                moment(flight.departure_airport_time).format("YYYY-MM-DD") === date &&
-                actualDatetime <= moment(flight.departure_airport_time)
-            );
-        }
+app.get("/flights", async (req, res) => {
+  try {
+    const { page = 1, count = 25, departure, arrival, date } = req.query;
+    const flights = await db.getAllFlights();
+    const actualDatetime = moment().utc();
+    let flightsFiltered = flights;
 
-        const startIndex = (page - 1) * count;
-        const selectedFlights = flightsFiltered.slice(startIndex, startIndex + count);
-
-        res.json({ flights: selectedFlights });
-    } catch (error) {
-        res.status(500).json({ message: "Error retrieving flights", error: error.message });
+    if (departure && !arrival && !date) {
+      flightsFiltered = flights.filter(
+        (flight) => flight.departure_airport_id === departure
+      );
+    } else if (arrival && !departure && !date) {
+      flightsFiltered = flights.filter(
+        (flight) => flight.arrival_airport_id === arrival
+      );
+    } else if (date && !departure && !arrival) {
+      flightsFiltered = flights.filter(
+        (flight) =>
+          moment(flight.departure_airport_time).format("YYYY-MM-DD") === date &&
+          actualDatetime <= moment(flight.departure_airport_time)
+      );
+    } else if (departure && arrival && !date) {
+      flightsFiltered = flights.filter(
+        (flight) =>
+          flight.departure_airport_id === departure &&
+          flight.arrival_airport_id === arrival
+      );
+    } else if (departure && date && !arrival) {
+      flightsFiltered = flights.filter(
+        (flight) =>
+          flight.departure_airport_id === departure &&
+          moment(flight.departure_airport_time).format("YYYY-MM-DD") === date &&
+          actualDatetime <= moment(flight.departure_airport_time)
+      );
+    } else if (arrival && date && !departure) {
+      flightsFiltered = flights.filter(
+        (flight) =>
+          flight.arrival_airport_id === arrival &&
+          moment(flight.departure_airport_time).format("YYYY-MM-DD") === date &&
+          actualDatetime <= moment(flight.departure_airport_time)
+      );
+    } else if (departure && arrival && date) {
+      flightsFiltered = flights.filter(
+        (flight) =>
+          flight.departure_airport_id === departure &&
+          flight.arrival_airport_id === arrival &&
+          moment(flight.departure_airport_time).format("YYYY-MM-DD") === date &&
+          actualDatetime <= moment(flight.departure_airport_time)
+      );
     }
+
+    const startIndex = (page - 1) * count;
+    const selectedFlights = flightsFiltered.slice(
+      startIndex,
+      startIndex + count
+    );
+
+    res.json({ flights: selectedFlights });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error retrieving flights", error: error.message });
+  }
 });
 
-
-app.get('/flights/:identifier', async (req, res) => {
-    try {
-        const flight = await db.getFlight(req.params.identifier);
-        res.json({ flight });
-    } catch (error) {
-        res.status(500).json({ message: "Error retrieving flight", error: error.message });
-    }
+app.get("/flights/:identifier", async (req, res) => {
+  try {
+    const flight = await db.getFlight(req.params.identifier);
+    res.json({ flight });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error retrieving flight", error: error.message });
+  }
 });
 
-app.post('/flights', async (req, res) => {
-    try {
-        const flightData = new FlightData(req.body);
-        await db.insertFlight(flightData);
-        res.status(201).json({ message: "Flight inserted successfully" });
-    } catch (error) {
-        res.status(500).json({ message: "An error occurred inserting the flight", error: error.message });
-    }
+app.post("/flights", async (req, res) => {
+  try {
+    const flightData = new FlightData(req.body);
+    await db.insertFlight(flightData);
+    res.status(201).json({ message: "Flight inserted successfully" });
+  } catch (error) {
+    res.status(500).json({
+      message: "An error occurred inserting the flight",
+      error: error.message,
+    });
+  }
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
