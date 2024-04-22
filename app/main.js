14ec074837e87b838467275c8d294cc745748e02
Matias Oliva <matiasolivab@uc.cl>
2024-04-21 22:41:26 -0400

    v10

--- app/main.js
+++ app/main.js
@@ -11,6 +11,9 @@ const jwtCheck = auth({
   audience: "https://dev-1op7rfthd5gfwdq8.us.auth0.com/api/v2/",
   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
   tokenSigningAlg: "RS256",
+  secret: process.env.AUTH0_SECRET,
+  algorithms: ["RS256"],
+  issuer: process.env.AUTH0_ISSUER_BASE_URL,
 });
 
 const corsOptions = {
@@ -171,7 +174,8 @@ app.post("/flights", async (req, res) => {
 
 app.get("/purchase", jwtCheck, async (req, res) => {
   try {
-    const user_id = "req.user.sub";
+    console.log(req)
+    const user_id = req.data.sub;
     const purchases = await db.getMyPurchases(user_id);
     res.json({ purchases });
   } catch (error) {
@@ -203,7 +207,7 @@ app.post("/flights/request", jwtCheck, async (req, res) => {
 
       await db.insertPurchase({
         flight_id: body.flight_id,
-        user_id: "req.user.sub",
+        user_id: req.user.sub,
         purchase_status: "pending",
         uuid: message.request_id,
         quantity: body.quantity,
