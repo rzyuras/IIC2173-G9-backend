e2f07e51b487eb9b53aef999a0a20c8016f54498
lolenn10 <127698491+lolenn10@users.noreply.github.com>
2024-04-21 23:57:12 -0400

    Revert "Develop"

--- app/main.js
+++ app/main.js
@@ -1,41 +1,41 @@
-const express = require('express');
-const mqtt = require('mqtt');
-const { v4: uuidv4 } = require('uuid');
-const moment = require('moment-timezone');
-const { auth } = require('express-oauth2-jwt-bearer');
-const cors = require('cors');
-const Database = require('./db');
-require('dotenv').config();
+const express = require("express");
+const mqtt = require("mqtt");
+const { v4: uuidv4 } = require("uuid");
+const moment = require("moment-timezone");
+const { auth } = require("express-oauth2-jwt-bearer");
+const cors = require("cors");
+const Database = require("./db");
+require("dotenv").config();
 
 const jwtCheck = auth({
-  audience: 'https://dev-1op7rfthd5gfwdq8.us.auth0.com/api/v2/',
+  audience: "https://dev-1op7rfthd5gfwdq8.us.auth0.com/api/v2/",
   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
-  tokenSigningAlg: 'RS256',
+  tokenSigningAlg: "RS256",
   secret: process.env.AUTH0_SECRET,
-  algorithms: ['RS256'],
+  algorithms: ["RS256"],
   issuer: process.env.AUTH0_ISSUER_BASE_URL,
 });
 
 const corsOptions = {
-  origin: '*',
+  origin: "*",
   allowHeaders: [
-    'Access-Control-Allow-Headers',
-    'Origin',
-    'Accept',
-    'X-Requested-With',
-    'Content-Type',
-    'Access-Control-Request-Method',
-    'Access-Control-Request-Headers',
-    'Auth',
+    "Access-Control-Allow-Headers",
+    "Origin",
+    "Accept",
+    "X-Requested-With",
+    "Content-Type",
+    "Access-Control-Request-Method",
+    "Access-Control-Request-Headers",
+    "Auth",
   ],
-  allowMethods: ['GET', 'HEAD', 'OPTIONS', 'PUT', 'POST', 'DELETE', 'PATCH'],
+  allowMethods: ["GET", "HEAD", "OPTIONS", "PUT", "POST", "DELETE", "PATCH"],
 };
 
 // Datos de conexión PostgreSQL
 const pgDbname = process.env.DATABASE_NAME;
 const pgUser = process.env.DATABASE_USER;
 const pgPassword = process.env.DATABASE_PASSWORD;
-const pgHost = 'db'; // O usa 'localhost' según tu configuración local
+const pgHost = "db"; // O usa 'localhost' según tu configuración local
 const mqttBroker = process.env.MQTT_BROKER;
 const mqttPort = process.env.MQTT_PORT;
 const mqttUser = process.env.MQTT_USER;
@@ -51,11 +51,11 @@ const client = mqtt.connect(`mqtt://${mqttBroker}:${mqttPort}`, {
   password: mqttPassword,
 });
 
-client.on('connect', () => {
-  console.log('Connected to MQTT Broker');
+client.on("connect", () => {
+  console.log("Connected to MQTT Broker");
 });
 
-client.on('error', (error) => {
+client.on("error", (error) => {
   console.error(`An error occurred: ${error}`);
 });
 
@@ -83,187 +83,184 @@ class FlightData {
   }
 }
 
-app.get('/flights', async (req, res) => {
+app.get("/flights", async (req, res) => {
   try {
-    const {
-      page = 1, count = 25, departure, arrival, date,
-    } = req.query;
+    const { page = 1, count = 25, departure, arrival, date } = req.query;
     const flights = await db.getAllFlights();
     const actualDatetime = moment().utc();
     let flightsFiltered = flights;
 
     if (departure && !arrival && !date) {
       flightsFiltered = flights.filter(
-        (flight) => flight.departure_airport_id === departure,
+        (flight) => flight.departure_airport_id === departure
       );
     } else if (arrival && !departure && !date) {
       flightsFiltered = flights.filter(
-        (flight) => flight.arrival_airport_id === arrival,
+        (flight) => flight.arrival_airport_id === arrival
       );
     } else if (date && !departure && !arrival) {
       flightsFiltered = flights.filter(
-        (flight) => moment(flight.departure_airport_time).format('YYYY-MM-DD') === date
-          && actualDatetime <= moment(flight.departure_airport_time),
+        (flight) =>
+          moment(flight.departure_airport_time).format("YYYY-MM-DD") === date &&
+          actualDatetime <= moment(flight.departure_airport_time)
       );
     } else if (departure && arrival && !date) {
       flightsFiltered = flights.filter(
-        (flight) => flight.departure_airport_id === departure
-          && flight.arrival_airport_id === arrival,
+        (flight) =>
+          flight.departure_airport_id === departure &&
+          flight.arrival_airport_id === arrival
       );
     } else if (departure && date && !arrival) {
       flightsFiltered = flights.filter(
-        (flight) => flight.departure_airport_id === departure
-          && moment(flight.departure_airport_time).format('YYYY-MM-DD') === date
-          && actualDatetime <= moment(flight.departure_airport_time),
+        (flight) =>
+          flight.departure_airport_id === departure &&
+          moment(flight.departure_airport_time).format("YYYY-MM-DD") === date &&
+          actualDatetime <= moment(flight.departure_airport_time)
       );
     } else if (arrival && date && !departure) {
       flightsFiltered = flights.filter(
-        (flight) => flight.arrival_airport_id === arrival
-          && moment(flight.departure_airport_time).format('YYYY-MM-DD') === date
-          && actualDatetime <= moment(flight.departure_airport_time),
+        (flight) =>
+          flight.arrival_airport_id === arrival &&
+          moment(flight.departure_airport_time).format("YYYY-MM-DD") === date &&
+          actualDatetime <= moment(flight.departure_airport_time)
       );
     } else if (departure && arrival && date) {
       flightsFiltered = flights.filter(
-        (flight) => flight.departure_airport_id === departure
-          && flight.arrival_airport_id === arrival
-          && moment(flight.departure_airport_time).format('YYYY-MM-DD') === date
-          && actualDatetime <= moment(flight.departure_airport_time),
+        (flight) =>
+          flight.departure_airport_id === departure &&
+          flight.arrival_airport_id === arrival &&
+          moment(flight.departure_airport_time).format("YYYY-MM-DD") === date &&
+          actualDatetime <= moment(flight.departure_airport_time)
       );
     }
 
     const startIndex = (page - 1) * count;
     const selectedFlights = flightsFiltered.slice(
       startIndex,
-      startIndex + count,
+      startIndex + count
     );
 
     res.json({ flights: selectedFlights });
   } catch (error) {
     res
       .status(500)
-      .json({ message: 'Error retrieving flights', error: error.message });
+      .json({ message: "Error retrieving flights", error: error.message });
   }
 });
 
-app.get('/flights/:identifier', async (req, res) => {
+app.get("/flights/:identifier", async (req, res) => {
   try {
     const flight = await db.getFlight(req.params.identifier);
     res.json({ flight });
   } catch (error) {
     res
       .status(500)
-      .json({ message: 'Error retrieving flight', error: error.message });
+      .json({ message: "Error retrieving flight", error: error.message });
   }
 });
 
-app.post('/flights', async (req, res) => {
+app.post("/flights", async (req, res) => {
   try {
     const flightData = new FlightData(req.body);
     await db.insertFlight(flightData);
-    res.status(201).json({ message: 'Flight inserted successfully' });
+    res.status(201).json({ message: "Flight inserted successfully" });
   } catch (error) {
     res.status(500).json({
-      message: 'An error occurred inserting the flight',
+      message: "An error occurred inserting the flight",
       error: error.message,
     });
   }
 });
 
-app.get('/purchase', jwtCheck, async (req, res) => {
+app.get("/purchase", jwtCheck, async (req, res) => {
   try {
-    console.log(req);
-    const user_id = req.auth.payload.sub;
+    console.log(req)
+    const user_id = req.auth.payload.sub;;
     const purchases = await db.getMyPurchases(user_id);
     res.json({ purchases });
   } catch (error) {
     res
       .status(500)
-      .json({ message: 'Error retrieving purchases', error: error.message });
+      .json({ message: "Error retrieving purchases", error: error.message });
   }
 });
 
-app.post('/flights/request', jwtCheck, async (req, res) => {
+app.post("/flights/request", jwtCheck, async (req, res) => {
   try {
-    const { body } = req;
+    const body = req.body;
 
-    if (body.type.includes('our_group_purchase')) {
+    if (body.type.includes("our_group_purchase")) {
       const flight = await db.getFlight(body.flight_id);
       const message = {
         request_id: uuidv4(),
-        group_id: '9',
+        group_id: "9",
         departure_airport: flight.departure_airport_name,
         arrival_airport: flight.arrival_airport_name,
         departure_time: moment(flight.departure_airport_time)
-          .tz('America/Santiago')
-          .format('YYYY-MM-DD HH:mm'),
-        datetime: moment().tz('America/Santiago').format('YYYY-MM-DD HH:mm'),
-        deposit_token: '',
+          .tz("America/Santiago")
+          .format("YYYY-MM-DD HH:mm"),
+        datetime: moment().tz("America/Santiago").format("YYYY-MM-DD HH:mm"),
+        deposit_token: "",
         quantity: body.quantity,
         seller: 0,
       };
-
+      console.log(req);
+      console.log(req.user);
       await db.insertPurchase({
         flight_id: body.flight_id,
         user_id: req.auth.payload.sub,
-        purchase_status: 'pending',
+        purchase_status: "pending",
         uuid: message.request_id,
         quantity: body.quantity,
       });
 
-      client.publish('flights/requests', JSON.stringify(message));
+      client.publish("flights/requests", JSON.stringify(message));
 
       // Si no hay error en la petición, responde con un mensaje de éxito (true)
       res.json({ success: true });
 
       // Otro Grupo
-    } else if (body.type.includes('other_group_purchase')) {
+    } else if (body.type.includes("other_group_purchase")) {
       console.log(body);
+      console.log(flight);
 
       const flight = db.getFlightBydata(
         body.departure_airport,
         body.arrival_airport,
-        body.departure_time,
+        body.departure_time
       );
 
       await db.insertPurchase({
         flight_id: flight.flight_id,
-        user_id: 'none',
-        purchase_status: 'pending',
+        user_id: "none",
+        purchase_status: "pending",
         uuid: body.uuid,
         quantity: body.quantity,
       });
     }
   } catch (error) {
     res.status(500).json({
-      message: 'An error occurred sending the request(API)',
+      message: "An error occurred sending the request(API)",
       error: error.message,
     });
   }
 });
 
-app.post('/flights/validation', async (req, res) => {
+app.post("/flights/validation", async (req, res) => {
   try {
-    const { body } = req;
-    const request_id = body.request_id;
+    const body = req.body;
+    console.log(body);
     const validation = Boolean(body.valid);
-    
-    const countPurchase = await db.getPurchase(request_id);
-    const flight = await db.getFlight(purchase.flight_id);
-    const flight_tickets = flight.flight_tickets;
-    if (countPurchase.quantity > flight_tickets) {
-      validation = false;
-    }
-
-
-    if (validation ) {
-      const purchaseData = await db.updatePurchase(body.request_id, 'approved');
+    console.log(validation);
+    if (validation) {
+      const purchaseData = await db.updatePurchase(body.request_id, "approved");
       await db.updateFlight(purchaseData.quantity, purchaseData.flight_id);
     } else {
-      await db.updatePurchase(body.request_id, 'rejected');
+      await db.updatePurchase(body.request_id, "rejected");
     }
   } catch (error) {
     res.status(500).json({
-      message: 'An error occurred sending the validation',
+      message: "An error occurred sending the validation",
       error: error.message,
     });
   }
